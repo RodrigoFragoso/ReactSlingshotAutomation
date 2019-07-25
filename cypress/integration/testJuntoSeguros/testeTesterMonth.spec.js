1b38198204d990e1dc0b60530466056bd965ba02
Rodrigo Local <Rodrigo Fragoso@NDCT030.ri.cwb.dc>
2019-07-25 13:38:06 -0300

    update, havia enviado a versao desatualizada.

--- cypress/integration/testJuntoSeguros/testeTesterMonth.spec.js
+++ cypress/integration/testJuntoSeguros/testeTesterMonth.spec.js
@@ -1,5 +1,5 @@
 describe('Form Month - Fuel Savings Analysis', function() {
-  beforeEach('Acessa a pagina fuel-savings', function() {
+  before('Acessa a pagina fuel-savings', function() {
     cy.visit('http://localhost:3000/fuel-savings')
   })
 
@@ -7,11 +7,11 @@ describe('Form Month - Fuel Savings Analysis', function() {
     cy.get('h2').should('contain','Fuel Savings Analysis')
   })
 
-    let newMpg = 500
-    let tradeMpg = 27
-    let newPpg = 45
-    let tradePpg = 155
-    let milesDriven = 900
+    let newMpg = '500.00'
+    let tradeMpg = '27.00'
+    let newPpg = '45.00'
+    let tradePpg = '155.00'
+    let milesDriven = '900.00'
 
   it('Preenche informações', function() {
     cy
@@ -23,19 +23,26 @@ describe('Form Month - Fuel Savings Analysis', function() {
       .get('select').select('Month')
   })
 
-  it('Verifica se carregou as colunas de resultados', () => {
-    cy.get('td').should('contain', 'Savings')
+  it('Verifica se carregou as colunas de resultados', function() {
+    cy
+      .get('td').should('contain', 'Savings')
+      .get('tbody').get('tr').get('td').should('contain','Monthly')
+      .get('tbody').get('tr').get('td').should('contain','1 Year')
+      .get('tbody').get('tr').get('td').should('contain','3 Year')
   })
 
-  it('Verifica se carregou as colunas de resultados', () => {
+  it('Verifica os resultados fixos', function(){
+    let resultMonthlyFixed = '$5,085.67'
+    let resultOneYearFixed = '$61,028.04'
+    let resultThreeYearFixed = '$183,084.12'
     cy
-      .get('tbody').get('td').should('contain','Monthly')
-      .get('tbody').get('td').should('contain','1 Year')
-      .get('tbody').get('td').should('contain','3 Year')
+      .get('td.savings').should('contain', resultMonthlyFixed)
+      .get('td.savings').should('contain', resultOneYearFixed)
+      .get('td.savings').should('contain', resultThreeYearFixed)
   })
 
   /*NÃO FINALIZEI O CALCULO DE VERIFICAÇÃO, POIS NÃO ENTENDI A REGRA DE NEGÓCIO, ATÉ CHEGAR AO CALCULO FINAL*/
-  it('Verifica os resultados', () => {
+  it('Verifica os resultados randomico', function() {
     let resultMonthly = (milesDriven/12)*52
     let resultOneYear = resultMonthly.toFixed(2)*12
     let resultThreeYear = resultMonthly.toFixed(2)*36
--- cypress/integration/testJuntoSeguros/testeTesterWeek.spec.js
+++ cypress/integration/testJuntoSeguros/testeTesterWeek.spec.js
@@ -1,5 +1,5 @@
 describe('Form Week - Fuel Savings Analysis', function() {
-  beforeEach('Acessa a pagina fuel-savings', function() {
+  before('Acessa a pagina fuel-savings', function() {
     cy.visit('http://localhost:3000/fuel-savings')
   })
 
@@ -7,11 +7,11 @@ describe('Form Week - Fuel Savings Analysis', function() {
     cy.get('h2').should('contain','Fuel Savings Analysis')
   })
 
-    let newMpg = 1000
-    let tradeMpg = 2
-    let newPpg = 50
-    let tradePpg = 300
-    let milesDriven = 700
+    let newMpg = '1000.00'
+    let tradeMpg = '2.00'
+    let newPpg = '50.00'
+    let tradePpg = '300.00'
+    let milesDriven = '700.00'
 
   it('Preenche informações', function() {
     cy
@@ -24,18 +24,25 @@ describe('Form Week - Fuel Savings Analysis', function() {
   })
 
   it('Verifica se carregou as colunas de resultados', () => {
-    cy.get('td').should('contain', 'Savings')
-  })
-
-  it('Verifica se carregou as colunas de resultados', () => {
     cy
+      .get('td').should('contain', 'Savings')
       .get('tbody').get('td').should('contain','Monthly')
       .get('tbody').get('td').should('contain','1 Year')
       .get('tbody').get('td').should('contain','3 Year')
   })
 
+  it('Verifica os resultados fixos', function(){
+    let resultMonthlyFixed = '$454,848.33'
+    let resultOneYearFixed = '$5,458,179.96'
+    let resultThreeYearFixed = '$16,374,539.88'
+    cy
+      .get('td.savings').should('contain', resultMonthlyFixed)
+      .get('td.savings').should('contain', resultOneYearFixed)
+      .get('td.savings').should('contain', resultThreeYearFixed)
+  })
+
   /*NÃO FINALIZEI O CALCULO DE VERIFICAÇÃO, POIS NÃO ENTENDI A REGRA DE NEGÓCIO, ATÉ CHEGAR AO CALCULO FINAL*/
-  it('Verifica os resultados', () => {
+  it('Verifica os resultados randomico', () => {
     let resultMonthly = (milesDriven/12)*52
     let resultOneYear = resultMonthly.toFixed(2)*12
     let resultThreeYear = resultMonthly.toFixed(2)*36
